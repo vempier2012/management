e90f44bd4b291c71f29e50879dbc1361d0a9b6af
vempier2012 <40449012+vempier2012@users.noreply.github.com>
2020-05-28 16:55:41 +0700

    Update 28/5/2020

---
+++ API Core/API/API/Entities/ActivityEventCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class ActivityEventCategory
+    {
+        public string classification_code { get; set; }
+        public string description { get; set; }
+        public DateTime created_date { get; set; }
+        public string created_by { get; set; }
+        public DateTime updated_date { get; set; }
+        public string updated_by { get; set; }
+        public int modified_count { get; set; }
+    }
+}
---
+++ API Core/API/API/Entities/EventList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class EventList
+    {
+        public string event_code { get; set; }
+        public string event_name { get; set; }
+    }
+}
